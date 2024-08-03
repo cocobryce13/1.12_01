8797f81f77cb7871a5fb84369cbca91fec9c7300
sokol <sokol@ASUS>
2024-08-03 14:45:28 +0500

    Добавьте файлы проекта.

---
+++ 1.12_01.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+#include<windows.h>
+#include<fstream>
+
+int main()
+{
+	SetConsoleCP(1251); 
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Rus");
+
+	std::ifstream f ("in.txt");
+	std::string str1;
+
+	if(f.is_open())
+	{
+		while (!f.eof())
+		{
+			f >> str1;
+			std::cout << str1 << std::endl;
+		}
+		f.close();
+	}
+	else
+	{
+		std::cout << "Не получилось открыть файл!";
+	}
+	std::cout << "\n\n\n";
+}
